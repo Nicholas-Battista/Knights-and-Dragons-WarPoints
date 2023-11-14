df90aa14c15ab2945cc669df084eee1d8b3dd6e9
Nicholas-Battista <battistan23@gmail.com>
2023-11-14 16:50:04 -0700

    Included type of attack & started elixr calcs
    changed html to include radio buttons for the type of attacks and to include elixr info. Added JS changes to apply type of attack.
    
    Also added increment and decrement buttons to elixr areas

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Knights-and-Dragons-WarPoints
+
+Include - elixr choices and how many and calc thru each one
+
+Include PA
+
+Include - how many points needed to earn and return PA or reg hits and elixr options 
+
+checkboxes for frenzy - should be default
--- js.js
+++ js.js
@@ -2,10 +2,23 @@ const freeEnergy = document.querySelector('.freeEnergy');
 const gemAmount = document.querySelector('.gems');
 const enterButton = document.querySelector('.enterButton');
 const totalPoints = document.querySelector('.totalPoints');
+const totalAttacks = document.querySelector('.totalAttacks');
 
 const SMALLPACK = [4, 10];
 const MEDIUMPACK = [20, 47];
 const BIGPACK = [40, 90];
+
+function increment () {
+    document.querySelector('.step').stepUp();
+}
+function decrement () {
+    document.querySelector('.step').stepDown();
+}
+
+function setElixrToZero () {
+    
+}
+
 
 enterButton.addEventListener('click', () => {
     const initialEnergy = freeEnergy.value;
@@ -20,10 +33,38 @@ enterButton.addEventListener('click', () => {
         currentGems -= BIGPACK[1];
     }
 
+    while (currentGems >= 47) {
+        totalEnergy += MEDIUMPACK[0];
+        currentGems -= MEDIUMPACK[1];
+    }
+
+    while (currentGems >= 10) {
+        totalEnergy += SMALLPACK[0];
+        currentGems -= SMALLPACK[1];
+    }
+
     console.log(currentGems);
     console.log(totalEnergy);
 
-    let warPoints = totalEnergy * 100;
+    if (document.querySelector('.regular').checked) {
+        let warPoints = totalEnergy * 912;
 
-    totalPoints.textContent = "War Points: " + warPoints;
+        totalPoints.textContent = "War Points (during frenzy): " + warPoints;
+
+        totalAttacks.textContent = "Total Attacks: " + totalEnergy;
+
+    }else if (document.querySelector('.pa').checked) {
+        let totalPowerAttacks = Math.floor(totalEnergy / 4);
+
+        let warPoints = totalPowerAttacks * 2712;
+
+        totalPoints.textContent = "PA War Points (during frenzy): " + warPoints;
+
+        totalAttacks.textContent = "Total Attacks: " + totalPowerAttacks;
+    }else {
+
+    }
+
+
+
 });
